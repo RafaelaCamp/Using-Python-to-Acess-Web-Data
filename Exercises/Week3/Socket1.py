9bafd660265778888019336af06abdf7d535852e
MeiBerry <campos.oli.rafaela@gmail.com>
2021-11-27 21:56:17 -0300

    Update Socket1.py

--- Exercises/Week3/Socket1.py
+++ Exercises/Week3/Socket1.py
@@ -1,8 +1,9 @@
+# Exploring the HyperText Transport Protocol
 import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
+cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode() #Make sure to CHANGE the code to retrieve the above URL - the values are different for each URL. 
 mysock.send(cmd)
 
 while True:
